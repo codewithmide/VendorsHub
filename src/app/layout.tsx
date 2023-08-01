e83f0704a6ccaa9bac8dbb9fbafa08ad7f6becd0
Olumide Micheal <olumidemicheal7@gmail.com>
2023-08-01 10:26:14 +0100

    added analytics and opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import './globals.css';
 import '../styles/fonts.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Vendors Hub',
   description: 'Resolve issues around hiring vendors for your event.',
+  openGraph: {
+    images: '/images/opengraph-image.png'
+  }
 }
 
 export default function RootLayout({
@@ -13,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='satoshi bg-primary text-black transition duration-300'>{children}</body>
+      <body className='satoshi bg-primary text-black transition duration-300'>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
